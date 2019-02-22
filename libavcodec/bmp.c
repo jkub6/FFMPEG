46a830769d05f05a61a960738f92982e12cea947
campbellmcgavin <campbellmcgavin@gmail.com>
2019-02-22 16:47:40 -0700

    added test comment to verify repo is working...
    look in bmp.c right after header file

--- libavcodec/bmp.c
+++ libavcodec/bmp.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+/* here is just a little test comment to prove the clone repo worked -campbell */
 #include <inttypes.h>
 
 #include "avcodec.h"
