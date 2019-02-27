fb1bd8872280e28c5cb2edd4c5ccf2a15bbb912f
campbellmcgavin <campbellmcgavin@gmail.com>
2019-02-26 18:14:57 -0700

    added comments for everything. tested with gradient, nature and 1024x1024 image. size is great. quality is passable.

--- libavcodec/cooldec.c
+++ libavcodec/cooldec.c
@@ -1,27 +1,14 @@
 /*
-test images with quality... this is a subjective test by the TA's
-test stuff with patterns...
-test one nature def
+Adaptation of the FFMPEG bmp codec to deffine a cool codec.
+The cool codec has the "magic number" of "co" It deffines
+width and height in the file header,
+followed by pixel data in RGB8 format (3 - red, 3 - green, 2 - blue).
 
-find a 1024*1024 image and make sure that it is roughly one megabyte
-
-
+Last modified 2/26/2019: Jacob Larkin and Campbell McGavin
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
 /*
- * BMP image format decoder
  * Copyright (c) 2005 Mans Rullgard
  *
  * This file is part of FFmpeg.
@@ -65,11 +52,13 @@ static int cool_decode_frame(AVCodecContext *avctx,
     unsigned int color_depth = 8;               /* the depth or quality of color <in bits> the .cool format */
     int width, height, i, j, n, linesize, ret;  /* helper fields to keep track of where we are at in reading the file */
     uint8_t *p_data;                            /* a pointer to where we are at in reading the data */
-    const uint8_t *buf0 = buf;
-    GetByteContext gb;
+    const uint8_t *buf_pos_0 = buf;             /* the position of the beginning of the buffer */
+    avctx->pix_fmt = AV_PIX_FMT_RGB8;           /* set the picture format to RGB8 color profile */
 
-    avctx->pix_fmt = AV_PIX_FMT_RGB8;
 
+    /*
+      Assure that the first two bytes of a .cool file are correct.
+    */
     if (bytestream_get_byte(&buf) != 'c' ||
         bytestream_get_byte(&buf) != 'o') {
         av_log(avctx, AV_LOG_ERROR, "bad magic number\n");
@@ -77,10 +66,16 @@ static int cool_decode_frame(AVCodecContext *avctx,
     }
 
 
+    /*
+      define the width and height of the image by looking into header info from .cool
+    */
     width  = bytestream_get_le32(&buf);
     height = bytestream_get_le32(&buf);
 
 
+    /*
+      assure that the dimensions are correctly set
+    */
     ret = ff_set_dimensions(avctx, width, height > 0 ? height : -(unsigned)height);
     if (ret < 0) {
         av_log(avctx, AV_LOG_ERROR, "Failed to set dimensions %d %d\n", width, height);
@@ -88,18 +83,21 @@ static int cool_decode_frame(AVCodecContext *avctx,
     }
 
 
-
+    /* assure that we can successfully get the buffer */
     if ((ret = ff_get_buffer(avctx, p, 0)) < 0)
         return ret;
 
+    /*  "I am not sure what this does." - Peter Jensen 02/26/2019 @ 15:12:37 */
     p->pict_type = AV_PICTURE_TYPE_I;
     p->key_frame = 1;
 
-    buf   = buf0 + hsize;
+    /* set buffer to the start of image data */
+    buf   = buf_pos_0 + hsize;
 
-    /* Line size in file multiple of 4 */
+    /* compute n to be used later on */
     n = ((avctx->width * color_depth + 31) / 8) & ~3;
 
+    /* verify that the image height is greater than 0 */
     if (height > 0) {
         p_data      = p->data[0] + (avctx->height - 1) * p->linesize[0];
         linesize = -p->linesize[0];
@@ -109,19 +107,20 @@ static int cool_decode_frame(AVCodecContext *avctx,
     }
 
 
-
+    /* read through the entire file pixel data and copy into the buffer*/
     for (i = 0; i < avctx->height; i++)
     {
         memcpy(p_data, buf, n);
         buf += n;
         p_data += linesize;
     }
-
     *got_frame = 1;
 
+    /* return the size of the buffer */
     return buf_size;
 }
 
+/* The struct in charge of setting up all properties for the decoding of a .cool file */
 AVCodec ff_cool_decoder = {
     .name           = "cool",
     .long_name      = NULL_IF_CONFIG_SMALL("long cool"),
--- libavcodec/coolenc.c
+++ libavcodec/coolenc.c
@@ -1,5 +1,13 @@
 /*
- * BMP image format encoder
+Adaptation of the FFMPEG bmp codec to deffine a cool codec.
+The cool codec has the "magic number" of "co" It deffines
+width and height in the file header,
+followed by pixel data in RGB8 format (3 - red, 3 - green, 2 - blue).
+
+Last modified 2/26/2019: Jacob Larkin and Campbell McGavin
+*/
+
+/*
  * Copyright (c) 2006, 2007 Michel Bardiaux
  * Copyright (c) 2009 Daniel Verkamp <daniel at drv.nu>
  *
@@ -27,6 +35,9 @@
 #include "internal.h"
 
 
+/*
+  Initialize information to successfully encode an image to the .cool format
+*/
 static av_cold int cool_encode_init(AVCodecContext *avctx)
 {
    if (avctx->pix_fmt == AV_PIX_FMT_RGB8)
@@ -39,61 +50,82 @@ static av_cold int cool_encode_init(AVCodecContext *avctx)
     return 0;
 }
 
+/*
+  Take data from internal form and putting it into a packet to be written to a file
+*/
 static int cool_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
                             const AVFrame *pict, int *got_packet)
 {
-    const AVFrame * const p = pict;
-    int n_bytes_image, n_bytes_per_row, n_bytes, i, hsize, ret;
-    uint32_t palette256[256];
-    int pad_bytes_per_row = 0;
-    int bit_count = avctx->bits_per_coded_sample;
-    uint8_t *ptr, *buf;
 
-#if FF_API_CODED_FRAME
-FF_DISABLE_DEPRECATION_WARNINGS
-    avctx->coded_frame->pict_type = AV_PICTURE_TYPE_I;
-    avctx->coded_frame->key_frame = 1;
-FF_ENABLE_DEPRECATION_WARNINGS
-#endif
+    const AVFrame * const p_pict = pict;                                /* a pointer to the AVFrame pict*/
+    int n_bytes_image, n_bytes_per_row, n_bytes, i, hsize, ret;         /* helper fields to keep track of where we are at in reading the file */
+    uint32_t palette256[256];                                           /* a palette of maximum available colors*/
+    int pad_bytes_per_row = 0;                                          /* the amount of padding per row */
+    int bit_count = avctx->bits_per_coded_sample;                       /* in our case this will always be 8 bits per pixel */
+    uint8_t *p_data, *buf;                                              /* pointers to the position in the data and the buffer */
 
+
+    /*  "I am not sure what this does." - Peter Jensen 02/26/2019 @ 15:12:37 */
+    #if FF_API_CODED_FRAME
+      FF_DISABLE_DEPRECATION_WARNINGS
+      avctx->coded_frame->pict_type = AV_PICTURE_TYPE_I;
+      avctx->coded_frame->key_frame = 1;
+      FF_ENABLE_DEPRECATION_WARNINGS
+    #endif
+
+    /* verify that the color quality is 8 bits per pixel */
     av_assert1(bit_count == 8);
     avpriv_set_systematic_pal2(palette256, avctx->pix_fmt);
 
+    /*
+    compute the number of total bytes, number of padding bytes and total image
+    bytes to be used in the buffer per row
+    */
     n_bytes_per_row = ((int64_t)avctx->width * (int64_t)bit_count + 7LL) >> 3LL;
     pad_bytes_per_row = (4 - n_bytes_per_row) & 3;
     n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row);
 
-    // STRUCTURE.field refer to the MSVC documentation for BITMAPFILEHEADER
-    // and related pages.
-
+    /* our header has a constant size of 10, so we don't need to adjust hsize */
     hsize = 10;
+
+    /* compute the total number of bytes, being the bytes from image && header */
     n_bytes = n_bytes_image + hsize;
+
+    /* allocate packet memory and assure no errors */
     if ((ret = ff_alloc_packet2(avctx, pkt, n_bytes, 0)) < 0)
         return ret;
-    buf = pkt->data;
-    bytestream_put_byte(&buf, 'c');                   // BITMAPFILEHEADER.bfType
-    bytestream_put_byte(&buf, 'o');                   // do.
-    bytestream_put_le32(&buf, avctx->width);          // BITMAPINFOHEADER.biWidth
-    bytestream_put_le32(&buf, avctx->height);         // BITMAPINFOHEADER.biHeight
 
-    // BMP files are bottom-to-top so we start from the end...
-    ptr = p->data[0] + (avctx->height - 1) * p->linesize[0];
+    /* set buffer to beginning of file header*/
+    buf = pkt->data;
+
+    /* create unique flag for the .cool image file type... "co" */
+    bytestream_put_byte(&buf, 'c');
+    bytestream_put_byte(&buf, 'o');
+
+    /* write the width and height of the image to the header info */
+    bytestream_put_le32(&buf, avctx->width);
+    bytestream_put_le32(&buf, avctx->height);
+
+    /* write the image data from bottom to top and then copy it to memory */
+    p_data = p_pict->data[0] + (avctx->height - 1) * p_pict->linesize[0];
     buf = pkt->data + hsize;
     for(i = 0; i < avctx->height; i++)
     {
-        memcpy(buf, ptr, n_bytes_per_row);
+        memcpy(buf, p_data, n_bytes_per_row);
         buf += n_bytes_per_row;
         memset(buf, 0, pad_bytes_per_row);
         buf += pad_bytes_per_row;
-        ptr -= p->linesize[0]; // ... and go back
+        p_data -= p_pict->linesize[0]; // ... and go back
     }
 
+    /* set the packet flags if initially set or flag key is set */
     pkt->flags |= AV_PKT_FLAG_KEY;
     *got_packet = 1;
     return 0;
 }
 
-AVCodec ff_bmp_encoder = {
+/* The struct in charge of setting up all properties for the encoding of a .cool file */
+AVCodec ff_cool_encoder = {
     .name           = "cool",
     .long_name      = NULL_IF_CONFIG_SMALL("long cool"),
     .type           = AVMEDIA_TYPE_VIDEO,
